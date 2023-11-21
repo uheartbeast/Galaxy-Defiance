a7f41aa86a6ee32cbb9aba43f8bf666937d5d063
Heartbeast <heartbeast.studios@gmail.com>
2023-11-21 10:53:37 -0700

    Finished part 2

---
+++ player_ship/ship.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var left_muzzle: Marker2D = $LeftMuzzle
+@onready var right_muzzle: Marker2D = $RightMuzzle
+@onready var spawner_component: SpawnerComponent = $SpawnerComponent as SpawnerComponent
+@onready var fire_rate_timer: Timer = $FireRateTimer
+@onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
+
+func _ready() -> void:
+	fire_rate_timer.timeout.connect(fire_lasers)
+	pass # Replace with function body.
+
+func fire_lasers() -> void:
+	spawner_component.spawn(left_muzzle.global_position)
+	spawner_component.spawn(right_muzzle.global_position)
+	scale_component.tween_scale()
---
+++ projectiles/laser.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+@onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
+@onready var flash_component: FlashComponent = $FlashComponent as FlashComponent
+
+func _ready() -> void:
+	scale_component.tween_scale()
+	flash_component.flash()
+	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
