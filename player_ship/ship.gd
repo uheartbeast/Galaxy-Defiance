aafaf149c2504f1eda31c1c311ef954aa218d0ef
Heartbeast <heartbeast.studios@gmail.com>
2023-11-21 11:07:10 -0700

    Finished part 3 (animating the ship)

--- player_ship/ship.gd
+++ player_ship/ship.gd
@@ -5,6 +5,10 @@ extends Node2D
 @onready var spawner_component: SpawnerComponent = $SpawnerComponent as SpawnerComponent
 @onready var fire_rate_timer: Timer = $FireRateTimer
 @onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
+@onready var animated_sprite_2d: AnimatedSprite2D = $Anchor/AnimatedSprite2D
+@onready var move_component: MoveComponent = $MoveComponent as MoveComponent
+@onready var flame_animated_sprite: AnimatedSprite2D = %FlameAnimatedSprite
+
 
 func _ready() -> void:
 	fire_rate_timer.timeout.connect(fire_lasers)
@@ -14,3 +18,17 @@ func fire_lasers() -> void:
 	spawner_component.spawn(left_muzzle.global_position)
 	spawner_component.spawn(right_muzzle.global_position)
 	scale_component.tween_scale()
+
+func _process(delta: float) -> void:
+	animate_the_ship()
+
+func animate_the_ship() -> void:
+	if move_component.velocity.x < 0:
+		animated_sprite_2d.play("bank_left")
+		flame_animated_sprite.play("bank_left")
+	elif move_component.velocity.x > 0:
+		animated_sprite_2d.play("bank_right")
+		flame_animated_sprite.play("bank_right")
+	else:
+		animated_sprite_2d.play("center")
+		flame_animated_sprite.play("center")
