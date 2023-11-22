9cab7f9d3d7da2f6c353f9c5d535c7034db1faa3
Heartbeast <heartbeast.studios@gmail.com>
2023-11-22 10:25:05 -0700

    Hitboxes and Hurtboxes

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -6,6 +6,11 @@ extends Node2D
 @onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
 @onready var flash_component: FlashComponent = $FlashComponent as FlashComponent
 @onready var shake_component: ShakeComponent = $ShakeComponent as ShakeComponent
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent as HurtboxComponent
+@onready var hitbox_component: HitboxComponent = $HitboxComponent as HitboxComponent
 
 func _ready() -> void:
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
+	hurtbox_component.hurt.connect(func(hitbox: HitboxComponent):
+		queue_free()
+	)
--- projectiles/laser.gd
+++ projectiles/laser.gd
@@ -3,8 +3,10 @@ extends Node2D
 @onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 @onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
 @onready var flash_component: FlashComponent = $FlashComponent as FlashComponent
+@onready var hitbox_component: HitboxComponent = $HitboxComponent as HitboxComponent
 
 func _ready() -> void:
 	scale_component.tween_scale()
 	flash_component.flash()
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
+	hitbox_component.hit_hurtbox.connect(queue_free.unbind(1))
