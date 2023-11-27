7e59b79aa0c427cf989e8330deebd5fc717b1d8e
Heartbeast <heartbeast.studios@gmail.com>
2023-11-27 11:15:30 -0700

    Finished part 9

--- components/onetime_animated_effect.gd
+++ components/onetime_animated_effect.gd
@@ -8,3 +8,4 @@ extends AnimatedSprite2D
 func _ready() -> void:
 	# Free this node when the animation is finished
 	animation_finished.connect(queue_free)
+	animation_looped.connect(queue_free)
--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
-@onready var stats_component: StatsComponent = $StatsComponent as StatsComponent
-@onready var move_component: MoveComponent = $MoveComponent as MoveComponent
+@onready var stats_component: = $StatsComponent as StatsComponent
+@onready var move_component: = $MoveComponent as MoveComponent
 @onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
-@onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
-@onready var flash_component: FlashComponent = $FlashComponent as FlashComponent
-@onready var shake_component: ShakeComponent = $ShakeComponent as ShakeComponent
-@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent as HurtboxComponent
-@onready var hitbox_component: HitboxComponent = $HitboxComponent as HitboxComponent
+@onready var scale_component: = $ScaleComponent as ScaleComponent
+@onready var flash_component: = $FlashComponent as FlashComponent
+@onready var shake_component: = $ShakeComponent as ShakeComponent
+@onready var hurtbox_component: = $HurtboxComponent as HurtboxComponent
+@onready var hitbox_component: = $HitboxComponent as HitboxComponent
+@onready var destroyed_component: = $DestroyedComponent as DestroyedComponent
 
 func _ready() -> void:
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
@@ -17,3 +18,4 @@ func _ready() -> void:
 		shake_component.tween_shake()
 	)
 	stats_component.no_health.connect(queue_free)
+	hitbox_component.hit_hurtbox.connect(destroyed_component.destroy.unbind(1))
