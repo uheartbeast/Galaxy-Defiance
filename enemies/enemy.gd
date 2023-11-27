0badf356f06664908f52f0fd92d5b6db08343d02
Heartbeast <heartbeast.studios@gmail.com>
2023-11-27 10:43:10 -0700

    Finished P8

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var stats_component: StateComponent = $StatsComponent as StateComponent
+@onready var stats_component: StatsComponent = $StatsComponent as StatsComponent
 @onready var move_component: MoveComponent = $MoveComponent as MoveComponent
 @onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 @onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
@@ -12,5 +12,8 @@ extends Node2D
 func _ready() -> void:
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
 	hurtbox_component.hurt.connect(func(hitbox: HitboxComponent):
-		queue_free()
+		scale_component.tween_scale()
+		flash_component.flash()
+		shake_component.tween_shake()
 	)
+	stats_component.no_health.connect(queue_free)
