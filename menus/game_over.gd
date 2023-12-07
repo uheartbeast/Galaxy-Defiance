00e95a035ef6920880b3ca6244c5d3a176e9de68
Heartbeast <heartbeast.studios@gmail.com>
2023-12-07 09:58:25 -0700

    Finished recording part 13

--- menus/game_over.gd
+++ menus/game_over.gd
@@ -1,5 +1,17 @@
 extends Control
 
+@export var game_stats: GameStats
+
+@onready var score_value: Label = %ScoreValue
+@onready var highscore_value: Label = %HighscoreValue
+
+func _ready() -> void:
+	if game_stats.score > game_stats.highscore:
+		game_stats.highscore = game_stats.score
+	score_value.text = str(game_stats.score)
+	highscore_value.text = str(game_stats.highscore)
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
+		game_stats.score = 0
 		get_tree().change_scene_to_file("res://menus/menu.tscn")
---
+++ resource_stash.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var game_stats: GameStats = load("res://game_stats.tres")
