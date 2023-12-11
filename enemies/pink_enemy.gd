45c8d04e1f08ababff7f25d1734dcad7e9fce802
Heartbeast <heartbeast.studios@gmail.com>
2023-12-11 10:41:21 -0700

    Part 15 pink enemy p1

---
+++ enemies/pink_enemy.gd
@@ -0,0 +1,24 @@
+class_name PinkEnemy
+extends Enemy
+
+@onready var states: Node = $States
+
+@onready var move_down_state: = %MoveDownState as TimedStateComponent
+@onready var move_side_state: = %MoveSideState as TimedStateComponent
+@onready var pause_state: = %PauseState as TimedStateComponent
+@onready var move_side_component: = %MoveSideComponent as MoveComponent
+
+func _ready() -> void:
+	super()
+	
+	for state in states.get_children():
+		state = state as StateComponent
+		state.disable()
+	
+	move_side_component.velocity.x = [-20, 20].pick_random()
+	
+	move_down_state.state_finished.connect(move_side_state.enable)
+	move_side_state.state_finished.connect(pause_state.enable)
+	pause_state.state_finished.connect(move_down_state.enable)
+	
+	move_down_state.enable()
