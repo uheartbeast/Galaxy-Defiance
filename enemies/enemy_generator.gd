c9d4f6b843564ba6961f1b2e9cd37bce72a17494
Heartbeast <heartbeast.studios@gmail.com>
2023-12-12 09:54:28 -0700

    P17 added difficulty

--- enemies/enemy_generator.gd
+++ enemies/enemy_generator.gd
@@ -4,6 +4,8 @@ extends Node2D
 @export var YellowEnemyScene: PackedScene
 @export var PinkEnemyScene: PackedScene
 
+@export var game_stats: GameStats
+
 var margin = 8
 var screen_width = ProjectSettings.get_setting("display/window/size/viewport_width")
 
@@ -14,10 +16,18 @@ var screen_width = ProjectSettings.get_setting("display/window/size/viewport_wid
 
 func _ready() -> void:
 	green_enemy_spawn_timer.timeout.connect(handle_spawn.bind(GreenEnemyScene, green_enemy_spawn_timer))
-	yellow_enemy_spawn_timer.timeout.connect(handle_spawn.bind(YellowEnemyScene, yellow_enemy_spawn_timer))
-	pink_enemy_spawn_timer.timeout.connect(handle_spawn.bind(PinkEnemyScene, pink_enemy_spawn_timer))
+	yellow_enemy_spawn_timer.timeout.connect(handle_spawn.bind(YellowEnemyScene, yellow_enemy_spawn_timer, 5.0))
+	pink_enemy_spawn_timer.timeout.connect(handle_spawn.bind(PinkEnemyScene, pink_enemy_spawn_timer, 10.0))
+	
+	game_stats.score_changed.connect(func(new_score: int):
+		if new_score > 10:
+			yellow_enemy_spawn_timer.process_mode = Node.PROCESS_MODE_INHERIT
+		if new_score > 50:
+			pink_enemy_spawn_timer.process_mode = Node.PROCESS_MODE_INHERIT
+	)
 
-func handle_spawn(enemy_scene: PackedScene, timer: Timer) -> void:
+func handle_spawn(enemy_scene: PackedScene, timer: Timer, time_offset: float = 1.0) -> void:
 	spawner_component.scene = enemy_scene
 	spawner_component.spawn(Vector2(randf_range(margin, screen_width-margin), -16))
-	timer.start()
+	var spawn_rate = time_offset / (0.5 + (game_stats.score * 0.01))
+	timer.start(spawn_rate + randf_range(0.25, 0.5))
